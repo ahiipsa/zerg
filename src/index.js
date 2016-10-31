550f0fd8be69b259da7601148ee44f0734357339
Sergey Karasev <ahiipsa@gmail.com>
2016-10-31 23:36:04 +0300

    fix jsdoc

--- src/index.js
+++ src/index.js
@@ -10,16 +10,16 @@ zerg.enable = filters.whiteList.enable;
 zerg.addTransport(transport.console);
 
 /**
- * @param {object} opt
- * @param {boolean} opt.console - disable/enable console transport
+ * @param {object} options - for config
+ * @param {boolean} options.console - disable/enable console transport
  * @return {undefined}
  */
-zerg.config = (opt) => {
-    if ({}.hasOwnProperty.call(opt, 'console')) {
+zerg.config = (options) => {
+    if ({}.hasOwnProperty.call(options, 'console')) {
 
         zerg.removeTransport(transport.console);
 
-        if (opt.console) {
+        if (options.console) {
             zerg.addTransport(transport.console);
         }
     }
